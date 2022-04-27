aeea707d8140ec572a1225c71bc0dcdaee3ffdd2
letuanhung <lthung1@cmcglobal.vn>
2022-04-27 22:52:09 +0700

    commit

--- src/main/java/ptit/QLKS/controller/impl/UploadControllerImpl.java
+++ src/main/java/ptit/QLKS/controller/impl/UploadControllerImpl.java
@@ -3,6 +3,7 @@ package ptit.QLKS.controller.impl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ptit.QLKS.controller.UploadController;
 import ptit.QLKS.service.UploadService;
@@ -10,7 +11,7 @@ import ptit.QLKS.vo.BaseResponse;
 
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 public class UploadControllerImpl implements UploadController {
 
     @Resource
