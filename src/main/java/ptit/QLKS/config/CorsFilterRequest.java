97c771b6a934c16940916a12f65446b9c9aa1823
letuanhung <lthung1@cmcglobal.vn>
2022-04-30 00:42:22 +0700

    commit

--- src/main/java/ptit/QLKS/config/CorsFilterRequest.java
+++ src/main/java/ptit/QLKS/config/CorsFilterRequest.java
@@ -74,7 +74,7 @@ public class CorsFilterRequest extends AbstractAuthenticationProcessingFilter im
             return;
         }
         if (paramUrl.startsWith("/login") || paramUrl.equalsIgnoreCase("/logout") || paramUrl.equalsIgnoreCase("/register")
-                || paramUrl.equalsIgnoreCase("/forgot-password") ||paramUrl.equalsIgnoreCase("/chat")) {
+                || paramUrl.equalsIgnoreCase("/forgot-password") ||paramUrl.equalsIgnoreCase("/upload")) {
             chain.doFilter(request, response);
         }
         else {
