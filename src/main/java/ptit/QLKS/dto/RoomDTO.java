d9d567ae25c120614b5eb09080739c182d38f143
letuanhung <lthung1@cmcglobal.vn>
2022-04-30 16:00:59 +0700

    commit

--- src/main/java/ptit/QLKS/dto/RoomDTO.java
+++ src/main/java/ptit/QLKS/dto/RoomDTO.java
@@ -1,5 +1,6 @@
 package ptit.QLKS.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -7,16 +8,21 @@ public class RoomDTO {
 
     private String id;
 
+    @JsonProperty("room_name")
     private String roomName;
 
     private long price;
 
+    @JsonProperty("electric_price")
     private long electricPrice;
 
+    @JsonProperty("water_price")
     private long waterPrice;
 
+    @JsonProperty("network_price")
     private long networkPrice;
 
+    @JsonProperty("max_number_people")
     private long maxNumberPeople;
 
     private String type;
@@ -27,12 +33,15 @@ public class RoomDTO {
 
     private String store;
 
+    @JsonProperty("booking")
     private boolean isBooking;
 
+    @JsonProperty("valid")
     private boolean isValid;
 
     private String images;
 
+    @JsonProperty("number_of_room")
     private int numberOfRoom;
 
 }
